ffcfec4130631f23335da53589e040bfb6ef6ce1
Jeroen de Haan <info.jdehaan@gmail.com>
2021-05-31 08:49:53 +0200

    Made small changes to fmod file

---
+++ Assets/Scripts/Utils/DontDestroyOnLoad.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    public static DontDestroyOnLoad Instance;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(Instance.gameObject);
+    }
+}
