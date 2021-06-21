826729a7188d9fe9b8fa829a30e0f0436fc0aa72
Jeroen de Haan <info.jdehaan@gmail.com>
2021-06-21 15:25:25 +0200

    Trying to fix sounds not playing

--- Assets/Scripts/Sound/SoundEmitter.cs
+++ Assets/Scripts/Sound/SoundEmitter.cs
@@ -14,42 +14,54 @@ public class SoundEmitter : MonoBehaviour
 
     [SerializeField] private playEvent _playEvent;
 
-    private void OnEnable()
-    {
-        _sound.Source = gameObject.AddComponent<AudioSource>();
-
-        _sound.Source.loop = _sound.Loop;
-        //_sound.Source.playOnAwake = _sound.PlayOnAwake;
-        _sound.Source.clip = _sound.Clip;
-        _sound.Source.volume = _sound.Volume;
-        _sound.Source.pitch = _sound.Pitch;
-        _sound.Source.spatialBlend = _sound.SpatialBlend;
-    }
-
     private void Awake()
     {
-        if (_playEvent == playEvent.Awake)
-            _sound.Source.Play();
+        //Debug.Log(_playEvent);
+        setSourceValues();
+        //if (_playEvent == playEvent.Awake)
+        //{
+        //    Debug.Log("Jghjhdgasjhdhfgasghjrdft");
+        //    _sound.Source.Play();
+        //}
+    }
+
+    private void Start()
+    {
+        Debug.Log(_sound.Clip.loadState);
+        //if(!_sound.Source.isPlaying)
+        //    _sound.Source.Play();
     }
 
     private void Update()
     {
-        switch (_playEvent)
-        {
-            case (playEvent) 1:
-                if (EventSystem.current.IsPointerOverGameObject())
-                {
-                    _sound.Source.Play();
-                    break;
-                }
-                break;
-            case (playEvent) 2:
-                if (EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0))
-                {
-                    _sound.Source.Play();
-                    break;
-                }
-                break;
-        }
+        //switch (_playEvent)
+        //{
+        //    case (playEvent) 1:
+        //        if (EventSystem.current.IsPointerOverGameObject())
+        //        {
+        //            _sound.Source.Play();
+        //            break;
+        //        }
+        //        break;
+        //    case (playEvent) 2:
+        //        if (EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0))
+        //        {
+        //            _sound.Source.Play();
+        //            break;
+        //        }
+        //        break;
+        //}
+    }
+
+    private void setSourceValues()
+    {
+        _sound.Source = gameObject.AddComponent<AudioSource>();
+        _sound.Source.clip = _sound.Clip;
+
+        _sound.Source.loop = _sound.Loop;
+        //_sound.Source.playOnAwake = _sound.PlayOnAwake;
+        _sound.Source.volume = _sound.Volume;
+        _sound.Source.pitch = _sound.Pitch;
+        _sound.Source.spatialBlend = _sound.SpatialBlend;
     }
 }
