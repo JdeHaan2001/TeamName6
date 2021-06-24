cd3f977bc9389a0eb43c0f02016529fb0cf775ac
Jeroen de Haan <info.jdehaan@gmail.com>
2021-06-25 00:26:15 +0200

    Implemented leaderboard

--- Assets/Scripts/UI/LeaderBoard.cs
+++ Assets/Scripts/UI/LeaderBoard.cs
@@ -1,38 +1,97 @@
+//Made by Jeroen de Haan
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using TMPro;
+using System.Linq;
 
 public class LeaderBoard : MonoBehaviour
 {
+    [SerializeField] private List<TextMeshProUGUI> _scoreTemplate = null;
+    [SerializeField] private List<TextMeshProUGUI> _names = null;
+
+    [SerializeField] private TextMeshProUGUI _scoreText = null;
+
     private QuestKeeper _questGetter = null;
 
-    private int _score = 0;
+    private int _score;
+    private string _name;
 
-    private List<string> line = new List<string>();
+    private List<int> line = new List<int>();
+    private List<string> namesLine = new List<string>();
+
+    private Dictionary<string, int> lines = new Dictionary<string, int>();
+    private Dictionary<string, int> templines = new Dictionary<string, int>();
 
     private void Awake()
     {
-        //_questGetter = GameObject.FindGameObjectWithTag("Player").GetComponent<QuestKeeper>();
-        //_score = _questGetter.Moral;
+        _questGetter = GameObject.FindGameObjectWithTag("Player").GetComponent<QuestKeeper>();
+        _score = _questGetter.Moral;
+        _name = PlayerPrefs.GetString("playerName", "ERROR_NO_NAME_FOUND");
+    }
+
+    private void Start()
+    {
         readFile();
+        _scoreText.text = _score.ToString();
+        Debug.Log(_score);
     }
 
     private void readFile()
     {
-        int counter = 0;
-        StreamReader sr = new StreamReader("D:/Documents/GitHub/TeamName6/LeaderBoard.txt");
-        line.Add(sr.ReadLine());
+        StreamReader reader = new StreamReader($"{Application.dataPath}/LeaderBoard.txt");
 
-        while (line != null)
+        string scoreString = reader.ReadLine();
+        int scoreText = scoreString != null ? int.Parse(scoreString) : -1;
+        string nameText = reader.ReadLine();
+        while (nameText != null)
         {
-            Debug.Log(line[counter]);
+            lines.Add(nameText, scoreText);
+            Debug.Log($"Read and added name: {nameText} and score: {scoreText} to dictionary\n Dictionary count: {lines.Count}");
 
-            line.Add(sr.ReadLine());
+            string tempScoreText = reader.ReadLine();
+            scoreText = tempScoreText != null ? int.Parse(tempScoreText) : -1;
+
+            nameText = reader.ReadLine();
+        }
+        reader.Close();
+
+        StreamWriter writer = new StreamWriter($"{Application.dataPath}/LeaderBoard.txt");
+
+        if (!lines.ContainsKey(_name))
+            lines.Add(_name, _score);
+        else
+        {
+            lines[_name] = _score;
+        }
+
+        Debug.Log($"Added name: {_name} and score: {_score} to dictionary\n Dictionary count: {lines.Count}");
+
+        var sortedDict = from entry in lines orderby entry.Value descending select entry;
+
+        int counter = 0;
+
+        foreach (KeyValuePair<string, int> dict in lines.OrderByDescending(key => key.Value))
+        {
+            writer.WriteLine(dict.Value);
+            writer.WriteLine(dict.Key);
+
+            _scoreTemplate[counter].text = dict.Value.ToString();
+            _names[counter].text = dict.Key;
+
             counter++;
         }
 
-        sr.Close();
-    }
+        if (lines.Count < 10)//20 because there are 10 scores and 10 names in the text file
+        {
+            for (int i = lines.Count; i < 10; i++)
+            {
+                _scoreTemplate[i].transform.parent.gameObject.SetActive(false);
+            }
+        }
 
+        writer.Close();
+    }
 }
